ff011c5d592aba82dc1ee2a508a67e00608fecf6
Oshea <user@Users-MacBook-Pro.local>
2019-07-31 23:18:17 -0500

    Continued working with js and strings mainly methods and functions.

--- exercises/212-number-joiners.js
+++ exercises/212-number-joiners.js
@@ -4,22 +4,16 @@
 // Examples:
 // numberJoinerWhile(1, 10) --> '1_2_3_4_5_6_7_8_9_10'
 // numberJoinerWhile(12, 14) --> '12_13_14'
-numberJoiner(start, end)
-let finalCount = start;
-while (start < end) {
-    console.log(finalCount+"_");
-    start++
+
+function numberJoinerWhile(start, end)
+{
+let myArray = [];
+while (start <= end) {
+  myArray.push(start);
+  start++;}
+  console.log(myArray.join('_'))
 }
-
-
-
-// A while loop that prints 1, 2, and 3
-let counterTwo = 1;
-while (counterTwo < 4) {
-  console.log(counterTwo);
-  counterTwo++;
-}
-
+numberJoinerWhile(1, 5)
 
 // >>>>  The join() method also joins all array elements into a string.
 
@@ -27,12 +21,23 @@ while (counterTwo < 4) {
 
 // >>>> var fruits = ["Banana", "Orange", "Apple", "Mango"];
 // >>>> document.getElementById("demo").innerHTML = fruits.join(" * ");
-  
-
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "numberJoinerFor" which does the same thing as "numberJoinerWhile",
 // except using a "for" loop internally.
+// for (i = 0; i < 5; i++) {
+//   text += "The number is " + i + "<br>";
+// }
+
+function numberJoinerFor (firstNumber, secondNumber)
+{
+let dosArray = [];
+for (num= firstNumber; num <= secondNumber; num++){
+  dosArray.push(num)
+}
+console.log(dosArray.join('_'))
+}
+numberJoinerFor(1, 9)
 
 
 
@@ -44,3 +49,14 @@ while (counterTwo < 4) {
 // numberJoinerFancy(1, 10) --> '1_2_3_4_5_6_7_8_9_10'
 // numberJoinerFancy(1, 5, '~') --> 1~2~3~4~5
 // numberJoinerFancy(3, 6, '***BANANAS***') --> 1***BANANAS***2***BANANAS***3
+
+function numberJoinerFancy (first, second, third)
+{
+// let tresArray =[];
+let stringIt = "";
+for (num= first; num <= second; num++){
+  stringIt+= num + third;
+}
+  console.log(stringIt)
+}
+numberJoinerFancy(3, 6, '***BANANAS***')
--- exercises/214-loopy-strings.js
+++ exercises/214-loopy-strings.js
@@ -29,14 +29,31 @@ console.log(reverse('Oshea is good'))
 // Example:
 // reverse("skoob") --> "books"
 
+
+// Now that we understand the problem, the input and expected output, we can break it down into steps.
+
+// 1. convert the string into an array with each character in the string as an element
+// 2. reverse the order of the elements in the array
+// 3. convert the reversed array back into a string
+// 4. return the reversed string
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "findLongestWord" that takes a string of words and returns
 // the longest word in that string. If there are multiple words with the same
 // maximum length return the first longest word.
 //
+
+function findLongestWord(str) {
+  var longestWord = str.split(' ').reduce(function(longest, currentWord) {
+    return currentWord.length > longest.length ? currentWord : longest;
+  }, "");
+  return longestWord;
+}
+findLongestWord("The quick brown fox jumped over the lazy dog");
+
+
 // Example:
 // findLongestWord('a book full of dogs') --> 'book'
-
 //function needs a string as input. then string becomes array. use .length method (?) or similar on array. 
 // Return first instance of that Number.
 
@@ -64,6 +81,27 @@ console.log(reverse('Oshea is good'))
 // capitalizeAll('hello world') --> 'Hello World'
 // capitalizeAll('every day is like sunday') --> 'Every Day Is Like Sunday'
 
+function capitalizeAll(str){
+var capitalize
+}
+
+capitalizeAll()
+
+// Take a string and convert it to an Array.  Use .capitalize on the first index of each index then add it to an empty string. 
+// Iterate through the array until the 
+
+function titleCase(str) {
+  var splitStr = str.toLowerCase().split(' ');
+  for (var i = 0; i < splitStr.length; i++) {
+      // You do not need to check if i is larger than splitStr length, as your for does that for you
+      // Assign it back to the array
+      splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+  }
+  // Directly return the joined string
+  return splitStr.join(' '); 
+}
+
+titleCase("I'm a little tea pot")
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -76,3 +114,18 @@ console.log(reverse('Oshea is good'))
 // split('a-b-c', '-') --> ['a', 'b', 'c']
 // split('APPLExxBANANAxxCHERRY', 'xx') --> ['APPLE', 'BANANA', 'CHERRY']
 // split('xyz', 'r') --> ['xyz']
+
+LOGIC >> Looping (for) with charAt << DONT RECOMMEND
+regular expression (RegEx) to find and replace
+
+
+
+
+
+function split(string, delimiter) {
+  var stringToChop = string
+  var restriction = delimiter
+  for (var index=0; index < string.length; index++)
+
+}
+
